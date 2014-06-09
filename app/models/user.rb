c0720ca484f6f996cde4625567704711bf6ad23b
mattstuddert <mstuddert@nuveer.com>
2014-06-09 13:26:40 +0100

    added Devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
-  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+
   has_many :subscriptions
   has_many :feeds, through: :subscriptions
 
@@ -12,5 +19,5 @@ class User < ActiveRecord::Base
   has_one :profile
 
   attr_accessible :first_name, :last_name
-  
+
 end
