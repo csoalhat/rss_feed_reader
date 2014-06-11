4bb7a244a53513da5ded7f68e10de75c807c1347
mattstuddert <mstuddert@nuveer.com>
2014-06-11 10:56:15 +0100

    started work on profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,8 @@ module ApplicationHelper
   def gravatar_image email
     hash_email = Digest::MD5.hexdigest(email.downcase)
     puts hash_email
-    image_tag "http://www.gravatar.com/avatar/#{hash_email}", width: 200
+    image_tag "http://www.gravatar.com/avatar/#{hash_email}", width: 200 
   end
 end
+
+# { style: "border-radius: 50%; margin-top: 30px;" }
